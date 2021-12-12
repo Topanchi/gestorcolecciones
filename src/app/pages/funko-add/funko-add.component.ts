688add8814a4ce8cb08bd494ab9f267a2f8c93ad
Francisco <franciscofs2000@gmail.com>
2021-12-12 19:37:19 -0300

    módulo carga de imágnenes

--- src/app/pages/funko-add/funko-add.component.ts
+++ src/app/pages/funko-add/funko-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-funko-add',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funko-add.component.css']
 })
 export class FunkoAddComponent implements OnInit {
+  public funkoForm: any = FormGroup;
+  imagenes: any[] = [];
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.funkoForm = this._formBuilder.group({
+      name: ['', Validators.compose([Validators.required])],
+      description: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  public onSubmit() {
+    console.log(this.funkoForm);
+  }
+
+  public cargarImagen(event: any){
+    console.log("cargar imagen");
+    let archivos = event.target.files;
+    let nombre = "jonathan";
+
+    for (let i = 0; i < archivos.length; i++) {
+
+      let reader = new FileReader();
+      reader.readAsDataURL(archivos[0]);
+      reader.onloadend = () => {
+        console.log(reader.result);
+        this.imagenes.push(reader.result);
+        /*
+        this.storageService.subirImagen(nombre + "_" + Date.now(), reader.result).then(urlImagen => {
+          let usuario = {
+            name: "jonathan",
+            nickName: "yonykikok",
+            password: "401325",
+            imgProfile: urlImagen
+          }
+          console.log(urlImagen);
+        });
+        */
+      }
+    }
   }
 
 }
