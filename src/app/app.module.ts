def9ed84c556362c2aa8ae4741b04831965599ce
Francisco <franciscofs2000@gmail.com>
2021-12-16 22:34:21 -0300

    capa servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AngularFireModule } from '@angular/fire/compat';
+//import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
 import { environment } from 'src/environments/environment';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { ToastrModule } from 'ngx-toastr';
-
 
 @NgModule({
   declarations: [
@@ -23,6 +27,9 @@ import { ToastrModule } from 'ngx-toastr';
     NgbModule,
     MatSliderModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
     NoopAnimationsModule,
     ToastrModule.forRoot(),
   ],
--- src/app/services/iron-man-funko.service.ts
+++ src/app/services/iron-man-funko.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+import { FunkoIronMan } from '../models/funko-iron-man';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IronManFunkoService {
 
-  constructor() { }
+  funkosRef: AngularFireList<any>;    
+  funkoRef: AngularFireObject<any>;   
+
+  constructor(private _db: AngularFireDatabase) { }
+
+  obtenerFunkosIronMan() {
+    this.funkosRef = this._db.list('funkos');
+    return this.funkosRef;
+  }
+
+  obtenerFunkoIronMan(id: string) {
+    this.funkoRef = this._db.object('funkos/' + id);
+    return this.funkoRef;
+  }
+
+  agregarFunkoIronMan(funko: FunkoIronMan) {
+    this.funkosRef.push({
+      name: funko.name,
+      description: funko.description,
+      collectionNumber: funko.collectionNumber,
+      imageComplete: funko.imageComplete,
+      imageSolo: funko.imageSolo
+    });
+  }
+
+  actualizarFunkoIronMan(funko: FunkoIronMan) {
+    this.funkoRef.update({
+      name: funko.name,
+      description: funko.description,
+      collectionNumber: funko.collectionNumber,
+      imageComplete: funko.imageComplete,
+      imageSolo: funko.imageSolo
+    });
+  }
+
+  eliminarFunkoIronMan(id: string) {
+    this.funkoRef = this._db.object('funkos/' + id);
+    this.funkoRef.remove();
+  }
 }
