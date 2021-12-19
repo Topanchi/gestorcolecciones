d6ed2457773f26483974ac84c47e338c9241d1cf
Francisco <franciscofs2000@gmail.com>
2021-12-19 17:56:46 -0300

    directiva dinámica

--- src/app/directives/headerimg.directive.ts
+++ src/app/directives/headerimg.directive.ts
@@ -4,24 +4,18 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
   selector: '[appHeaderimg]'
 })
 export class HeaderimgDirective {
-  @Input() name: string;
+
 
   constructor(private el: ElementRef,
               private renderer: Renderer2) {
-    console.log(this.name);
-    this.name = "Marvel";
-    switch (this.name) {
-      case 'Marvel':
-        this.renderer.addClass(this.el.nativeElement, 'fondo');
-        break;
-      default:
-        this.renderer.addClass(this.el.nativeElement, 'fondo');
-        break;
+   
+    this.renderer.addClass(this.el.nativeElement, 'fondo');
 
-    }
-    
-    
+
   }
+    
+    
+  
 
           
 }
