6bab30833ffcdc7eb9334c3b86c4ae0e3c65e0df
Francisco <franciscofs2000@gmail.com>
2022-01-05 15:22:18 -0300

    guard para sesión

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import {map, switchMap} from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import Swal from 'sweetalert2';
@@ -10,32 +10,34 @@ import Swal from 'sweetalert2';
 })
 export class AuthGuard implements CanActivate {
 
-  userLogged = this._autService.getUserLoggedStatus();
-  constructor(private _autService: AuthService, private _router: Router) {}
-  //Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const user = this.userLogged;
-      const isAuth = user ? true : false;
-      if(!isAuth){
-        Swal.fire({
-          title: 'Debe iniciar sesión',
-          showDenyButton: false,
-          showCancelButton: false,
-          confirmButtonText: 'Sí',
-          denyButtonText: `No`,
-        }).then((result) => {
-          
-          if (result.isConfirmed) {
-            this._router.navigate(['/']);
-          } 
-        })
-      }
+  constructor(private _router: Router,
+              private _afauth: AngularFireAuth) {}
 
-      return isAuth;
-      
-    
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return new Promise((resolve, reject) => {
+        this._afauth.onAuthStateChanged((user) => {
+          if (user) {
+            resolve(true);
+          } else {
+            console.error('User is not logged in');
+            resolve(false);
+            Swal.fire({
+              title: 'Debe iniciar sesión',
+              showDenyButton: false,
+              showCancelButton: false,
+              confirmButtonText: 'Sí',
+              denyButtonText: `No`,
+            }).then((result) => {
+              
+              if (result.isConfirmed) {
+                this._router.navigate(['/login']);
+              } 
+            })
+          }
+        });
+      });
   }
   
 }
